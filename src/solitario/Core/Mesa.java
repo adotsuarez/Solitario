33eff0badab775d972943ac7d35681a5cf54e85f
Iago <Iago@192.168.1.33>
2020-04-14 22:54:58 +0200

    Se han añadido los metodos de movimientos en la clase mesa.

--- src/solitario/Core/Mesa.java
+++ src/solitario/Core/Mesa.java
@@ -23,6 +23,7 @@ clase para representar la mesa de juego, y en particular de los métodos que se 
  */
 package solitario.Core;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 /**
@@ -37,4 +38,87 @@ import java.util.Stack;
 public class Mesa {
     private Stack<Carta> [][] montonInterior;
     private Stack<Carta> [] montonExterior;
+    Scanner en = new Scanner(System.in);
+    public void MovimientoInterior(){
+
+        int c0;
+        int c1;
+        int f0;
+        int f1;
+        do{
+            System.out.println("Introduce la fila y la columna de la carta a mover");
+            c0 = (Integer.parseInt(en.nextLine())-1);
+            c1 = (Integer.parseInt(en.nextLine())-1);
+            System.out.println("Introduce la fila y la columna a donde mover la carta");
+            f0= (Integer.parseInt(en.nextLine())-1);
+            f1= (Integer.parseInt(en.nextLine())-1);
+            
+        }while(c0> -1&&c1> -1&& f0> -1&& f1> -1);
+        if(!montonInterior[c0][c1].empty()||!montonInterior[f0][f1].empty()){
+            if (montonInterior[c0][c1].peek().numero == 7){//supongo que carta tiene el atributo numero
+                if(((montonInterior[c0][c1].peek().numero + 3) == montonInterior[f0][f1].peek().numero) && (montonInterior[c0][c1].peek().palo == montonInterior[f0][f1].peek().palo)){
+                    montonInterior[f0][f1].push(montonInterior[c0][c1].pop());
+                }else{
+                    System.out.println("Movimiento no valido");
+                }
+            }else{
+                if(((montonInterior[c0][c1].peek().numero + 1)== montonInterior[f0][f1].peek().numero)&&(montonInterior[c0][c1].peek().palo == montonInterior[f0][f1].peek().palo)){
+                    montonInterior[f0][f1].push(montonInterior[c0][c1].pop());
+                }else{
+                    System.out.println("Movimiento no valido");
+                }
+            }
+        }else{
+            System.out.println("El monton seleccionado o el monton de destino esta vacio");
+        }
+        System.out.println(toString());
+    }
+    public void MovimientoExterior(){
+        int c0;
+        int c1;
+        int f;
+        do{
+            System.out.println("Introduce la columna y la fila de la carta a mover");
+            c0 = (Integer.parseInt(en.nextLine()) - 1);
+            c1 = (Integer.parseInt(en.nextLine()) - 1);
+            
+            System.out.println("Introduce la fila del monton exterior");
+            f = (Integer.parseInt(entrada.nextLine()) - 1);
+            
+        }while(c0> -1&&c1> -1&& f> -1);
+        
+        if(montonInterior[c0][c1].peek().numero == 1){
+            
+            if(montonExterior[f].empty()){
+                
+                montonExterior[f].push(montonInterior[c0][c1].pop());
+            }else{
+                System.out.println("Solo se pueden colocar Ases en posiciones vacias");
+            }
+        }else{
+            if(!montonExterior[f].empty()){
+                
+                if(montonExterior[f].peek().numero == 7){
+                    
+                    if ((montonInterior[c0][c1].peek().numero == montonExterior[f].peek().numero + 3) && (montonInterior[c0][c1].peek().palo == montonExterior[f].peek().palo)){
+                        
+                        montonExterior[f].push(montonInterior[c0][c1].pop());
+                    }else{
+                    System.out.println("Movimiento Invalido");
+                    }
+                }else{
+                    
+                  if((montonInterior[c0][c1].peek().numero == montonExterior[f].peek().numero + 1) && (montonInterior[c0][c1].peek().palo == montonExterior[f].peek().palo)){
+                      
+                    montonExterior[f].push(montonInterior[c0][c1].pop());
+                }else{
+                    System.out.println("Movimiento Invalido");
+                }
+                }
+            }else{
+                System.out.println("Monton vacio");
+            }
+        }
+        System.out.println(toString());
+    }
 }
